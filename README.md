0d605de4fef7b25ff4c5d1a2958d9ea9de75cabb
Baraka <barakaarunga@gmail.com>
2023-11-03 14:12:38 +0300

    Changed the way scores are reflected   Added playerscore and computer score display   Removed autopopulating list after every round Added a winner element that announces winner with reset game button

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# rock-paper-scissors
+Rock, paper, scissors project from The Odin Project frontedn course.
+
+The aim of this project was to learn DOM manupulation, JS logic and git basic i.e branching and merging.
+
+The project took bout 20hrs to complete.
--- script.js
+++ script.js
@@ -1,55 +1,65 @@
 const contentWrapper = document.querySelector('.content-wrapper');
 const btnWrapper = document.querySelector('.btn-wrapper');
 const resultsList = document.querySelector('#results-wrapper');
+const result = document.querySelector('.result');
+const winnerWrapper = document.querySelector('.winner-wrapper');
+const playerScoreDisplay = document.querySelector('.player-score');
+const computerScoreDisplay = document.querySelector('.computer-score'); 
 
 let playerScore = 0;
 let computerScore = 0;
 
+//update score displays
+function updateScoreDisplay(){
+  playerScoreDisplay.textContent = `Player: ${playerScore}`;
+  computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+}
+
 function playRound(playerChoice) {
+
   // Get computer choice
   const choices = ["rock", "paper", "scissors"];
   const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-  console.log(computerChoice);
-  console.log(playerChoice);
-
+  
   // Pick a round winner
   if (playerChoice === computerChoice) {
-    const tieEntry = document.createElement('ul');
-    tieEntry.textContent = `You:${playerChoice} | PC:${computerChoice} | It's a tie :O`;
-    resultsList.appendChild(tieEntry);
+    result.textContent = `${playerChoice} ties with ${computerChoice} :0`;
   } else if (
     (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "rock" && computerChoice === "scissors") ||
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
-    const winEntry = document.createElement("li");
-    winEntry.textContent = `You:${playerChoice} | PC:${computerChoice} | You Win :D`;
-    resultsList.appendChild(winEntry);
+    result.textContent = `${playerChoice} beats ${computerChoice} :D`;
     playerScore++;
     console.log(playerScore);
   } else {
-    const lossEntry = document.createElement('ul');
-    lossEntry.textContent = `You:${playerChoice} | PC:${computerChoice} | You Lose :/`;
-    resultsList.appendChild(lossEntry);
+    result.textContent = `${playerChoice} loses to ${computerChoice} :/`;
     computerScore++;
     console.log(computerScore);
   }
 
-  // Game plays until one reaches 5 points
-  if (playerScore === 5 || computerScore === 5) {
-    if (playerScore > computerScore) {
-      const victoryEntry = document.createElement('li');
-      victoryEntry.textContent = "Winner, winner, chicken dinner!";
-      resultsList.appendChild(victoryEntry);
-    } else {
-      const loserEntry = document.createElement('li');
-      loserEntry.textContent = "You lose. Maybe try tic tac toe :P";
-      resultsList.appendChild(loserEntry);
-    }
-    computerScore = 0;
-    playerScore = 0;
-    return;
+  //lower winner wrapper
+  function lowerWinnerWrapper () {
+    winnerWrapper.setAttribute('style', 'transform:translateY(100%); duration:0.5s; ease-out;')
   }
+
+  // GWinner announced when one reaches 5 points
+   if (playerScore === 5 || computerScore === 5) {
+    if (playerScore > computerScore) {
+      const victoryEntry = document.createElement('p');
+      victoryEntry.textContent = "Winner, winner, chicken dinner!";
+      winnerWrapper.appendChild(victoryEntry);
+      lowerWinnerWrapper();
+    } else {
+      const loserEntry = document.createElement('p');
+      loserEntry.textContent = "You lose, maybe try tic tac toe :P";
+      winnerWrapper.appendChild(loserEntry);
+      lowerWinnerWrapper();
+    }
+  }
+
+    updateScoreDisplay();
+  
 }
 
 // Play round
@@ -61,3 +71,16 @@ paperBtn.addEventListener('click', () => playRound("paper"));
 
 const scissorsBtn = document.querySelector('#scissors-btn');
 scissorsBtn.addEventListener('click', () => playRound("scissors"));
+
+//Restart game
+function restartGame() {
+  computerScore = 0;
+  playerScore = 0;
+  result.textContent = "First to 5 wins";
+  updateScoreDisplay();
+  winnerWrapper.removeChild(winnerWrapper.lastChild);
+  winnerWrapper.setAttribute('style','transform:translateY(-100%); duration:0.5s; ease-out;');
+}
+
+const restartBtn = document.querySelector('.restart-btn');
+restartBtn.addEventListener('click', () => restartGame());
